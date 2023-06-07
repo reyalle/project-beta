569eaaf189cb5cc45801180f254bc9ae5262eddf
Rob <rclaus29@gmail.com>
2023-06-07 16:06:19 -0400

    Fixed index

--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -42,50 +42,6 @@ async function loadCustomers() {
 
 loadCustomers();
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
-async function loadSalesPeople() {
-  const response = await fetch('http://localhost:8090/api/salespeople/');
-  if (response.ok) {
-    const data = await response.json();
-    root.render(
-      <React.StrictMode>
-        <App salespeople={data.salespeople} />
-      </React.StrictMode>
-    )
-  } else {
-    console.error(response)
-  }
-}
-
-loadSalesPeople();
-
-async function loadCustomers() {
-  const response = await fetch ('http://localhost:8090/api/customers/');
-  if (response.ok) {
-    const data = await response.json();
-    root.render(
-      <React.StrictMode>
-        <App customer={data.customer} />
-      </React.StrictMode>
-    )
-  } else {
-    console.error(response)
-  }
-}
-
-loadCustomers();
-
 async function loadAutomobiles() {
   const response = await fetch ('http://localhost:8100/api/automobiles/');
   if (response.ok) {
